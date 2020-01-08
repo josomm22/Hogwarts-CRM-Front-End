b144654eba15a55af6b0b0990be80e4788ac39e7
josomm22 <josommefeld@icloud.com>
2020-01-08 22:13:35 +0200

    get student info by id is now working

--- src/api/api.js
+++ src/api/api.js
@@ -24,4 +24,27 @@ function getTable() {
     });
 };
 
-getTable()
+function getStudentData(studentID){
+    axios.get(baseUrl + '/students/' + studentID, {
+    }).then(response => {
+        // console.log('response is : ' + response.data)
+        return response.data;
+    }).then(data => {
+        console.log(data.student_details);
+
+    }).catch(function (error) {
+        if (error.response) {
+            console.log(error.response.headers);
+        }
+        else if (error.request) {
+            console.log(error.request);
+        }
+        else {
+            console.log(error.message);
+        }
+        console.log(error.config);
+    });
+};
+
+getTable();
+getStudentData(1001);
