2aea7bd5d92653d2475e4ea9ac9490ff9c2655a5
josomm22 <josommefeld@icloud.com>
2020-01-14 18:14:28 +0200

    dropdowns are now working

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { getStudentData, getSkillzObject, getCoursesObject } from '../api/api';
-// import Select from 'react-select';
+import Select from 'react-select';
 import { Inputboxes, Skillbox } from './studentComponents';
-
 
 
 class Studendetails extends React.Component {
@@ -12,14 +11,20 @@ class Studendetails extends React.Component {
             isLoading: true,
             studentID: this.props.match.params.id,
             studentData: null,
+            firstName: null,
+            lastName: null,
+            existingSkills: null,
+            desiredSkills: null,
+            courseInterests: null,
             skillsArr: null,
             coursesArr: null,
             selectedOption: null,
+            options: null,
         };
+        this.handleChange = this.handleChange.bind(this);
 
     };
     componentDidMount() {
-        // const id = this.props.match.params.id
         this.loadStudentData(this.state.studentID);
         this.loadCourses()
     }
@@ -27,15 +32,44 @@ class Studendetails extends React.Component {
     async loadStudentData(id) {
         return await getStudentData(id)
             .then(result =>
-                this.setState({ studentData: result, isLoading: false }))
+                this.setState({
+                    studentData: result,
+                    firstName: result.first_name,
+                    lastName: result.last_name,
+                    createdOn: result.date_created,
+                    updatedOn: result.last_updated,
+                    existingSkills: result.existing_skillz,
+                    desiredSkills: result.desired_skillz,
+                    courseInterests: result.course_interests,
+                    id: result.id,
+                    isLoading: false
+                }))
             .then(this.loadSkills())
 
 
     };
     async loadSkills() {
         return await getSkillzObject()
-            .then(result =>
-                this.setState({ skillsArr: result }))
+            .then(result => {
+                this.setState({ skillsArr: result })
+                let newOptions = [];
+                for (let [key, value] of Object.entries(result)) {
+                    let option = {};
+                    option.value = key;
+                    option.label = value;
+                    newOptions.push(option)
+                };
+                this.setState({ options: newOptions })
+            }
+            )
+    }
+    convertToValueLabel(obj) {
+        let option = {};
+        option.value = obj[0];
+        option.label = this.state.skillsArr[obj[0]];
+        return option;
+
+
     }
     async loadCourses() {
         return await getCoursesObject()
@@ -43,30 +77,37 @@ class Studendetails extends React.Component {
                 this.setState({ coursesArr: result }))
     }
 
-    fillTextBox(key) {
-        if (this.state.isLoading == true) {
-            return 'Loading...'
+    handleChange(selectedOption, i, array) {
+        if (selectedOption.value) {
+            console.log('index is ' + i + 'value is ' + selectedOption.value);
+            let oldArr = this.state[array]
+            let oldVal = oldArr[i]
+            oldVal[0] = parseInt(selectedOption.value)
+            oldArr[i] = oldVal
+            this.setState({ array: oldArr })
+
+        } else {
+            console.log('index is ' + i + 'value is ' + selectedOption.target.value)
+            let oldArr = this.state[array]
+            let oldVal = oldArr[i]
+            oldVal[1] = parseInt(selectedOption.target.value)
+            oldArr[i] = oldVal
+            this.setState({ array: oldArr })
         }
-        else {
-            return this.state.studentData[key]
-        }
-    };
-    handleChange = selectedOption => {
-        this.setState({ selectedOption });
-        console.log(`Option selected:`, selectedOption);
+
     };
     render() {
-
+        const { firstName, lastName, createdOn, updatedOn, id, existingSkills, desiredSkills, courseInterests } = this.state;
         return (
             <div>
                 <h1>Student Details</h1>
-                <Inputboxes id={'firstName'} readOnly={true} nameTag={'First Name'} type={'text'} value={this.fillTextBox('first_name')} />
-                <Inputboxes id={'lastName'} readOnly={true} nameTag={'Last Name'} type={'text'} value={this.fillTextBox('last_name')} />
+                <Inputboxes id={'firstName'} readOnly={true} nameTag={'First Name'} type={'text'} value={firstName} />
+                <Inputboxes id={'lastName'} readOnly={true} nameTag={'Last Name'} type={'text'} value={lastName} />
                 <br />
-                <Inputboxes id={'createdOn'} readOnly={true} nameTag={'Created On'} type={'text'} value={this.fillTextBox('date_created')} />
-                <Inputboxes id={'updatedOn'} readOnly={true} nameTag={'Updated on'} type={'text'} value={this.fillTextBox('last_updated')} />
+                <Inputboxes id={'createdOn'} readOnly={true} nameTag={'Created On'} type={'text'} value={createdOn} />
+                <Inputboxes id={'updatedOn'} readOnly={true} nameTag={'Updated on'} type={'text'} value={updatedOn} />
                 <br />
-                <Inputboxes id={'ID'} readOnly={true} nameTag={'Student ID'} type={'text'} value={this.fillTextBox('id')} />
+                <Inputboxes id={'id'} readOnly={true} nameTag={'Student ID'} type={'text'} value={id} />
 
                 <div>
                     <h5>Current skills</h5>
@@ -76,22 +117,37 @@ class Studendetails extends React.Component {
                         &&
                         <ol>
 
-                            {this.state.studentData.existing_skillz.map(arr =>
-                                <li>{this.state.skillsArr[arr[0]]} {arr[1]}/5</li>
+                            {existingSkills.map((arr, i) =>
+                                <li>
+                                    <Select
+                                        onChange={(event) => this.handleChange(event, i, 'existingSkills')}
+                                        options={this.state.options}
+                                        value={this.convertToValueLabel(arr)}
+                                    />
+                                    <input type={'number'} onChange={(event) => this.handleChange(event, i, 'existingSkills')} step={1} min={1} max={5} readOnly={false} value={arr[1]} index={i}
+                                    />
+                                </li>
                             )}
                         </ol>
                     }
                 </div>
                 <div>
-                    <h5>Desired skills</h5>
+                    <h5>Desired Skills</h5>
                     {this.state.skillsArr
                         &&
                         this.state.studentData
                         &&
                         <ol>
 
-                            {this.state.studentData.desired_skillz.map(arr =>
-                                <li>{this.state.skillsArr[arr[0]]} {arr[1]}/5</li>
+                            {desiredSkills.map((arr, i) =>
+                                <li>
+                                    <Select
+                                        onChange={this.handleChange}
+                                        options={this.state.options}
+                                        value={this.convertToValueLabel(arr, 0)}
+                                    />
+                                    <input type={'number'} step={1} readOnly={true} value={arr[1]} />
+                                </li>
                             )}
                         </ol>
                     }
@@ -104,7 +160,7 @@ class Studendetails extends React.Component {
                         &&
                         <ol>
 
-                            {this.state.studentData.course_interests.map(arr =>
+                            {courseInterests.map(arr =>
                                 <li>{this.state.coursesArr[arr]}</li>
                             )}
                         </ol>
